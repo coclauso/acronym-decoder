49102e8374313668a4e116f95643c747726a7065
C. Clauson <coclauso@microsoft.com>
2019-09-19 20:05:54 -0700

    Remove MVC and default content, add stand-in index.html

--- AcronymDecoder/Startup.cs
+++ AcronymDecoder/Startup.cs
@@ -44,21 +44,14 @@ namespace AcronymDecoder
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
         }
     }
 }
