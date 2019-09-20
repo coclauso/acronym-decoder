0eba105ae76fbdde408ff755e75ae315a8a60b6e
C. Clauson <coclauso@microsoft.com>
2019-09-19 19:44:54 -0700

    Commit default ASP.NET Core project

---
+++ AcronymDecoder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AcronymDecoder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
